069ee4800aa69fe3987e32c2e4783b6371b32ef8
king68312 <t.ksrzsr0330@gmail.com>
2025-06-27 10:49:24 +0900

    Dice game: initial revision

---
+++ Dice.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int main(){
+  int Die1, Die2;
+  int sum = 0;
+  srand((unsigned)time(NULL));
+  cout << "Rolling dice..." << endl;
+  Die1 = rand()%6+1;//rand()%6で0から5の値を出し、それらすべてを+1する
+  cout << "Die 1: " << Die1 << endl;
+  Die2 = rand()%6+1;
+  cout << "Die 2: " << Die2 << endl;
+  sum = Die1+Die2;
+  cout << "Total value: " << sum << endl;
+}
